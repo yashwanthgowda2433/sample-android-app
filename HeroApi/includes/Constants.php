f3779e158fb4596b3dc0a7f8fe5d135be38638eb
Yashwanth Gowda <48586625+yashwanthgowda2433@users.noreply.github.com>
2021-05-18 16:08:24 +0530

    Add files via upload

---
+++ HeroApi/includes/Constants.php
@@ -0,0 +1,11 @@
+<?php 
+
+	/*
+	* Created by Belal Khan
+	* website: www.simplifiedcoding.net 
+	*/
+	
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'localhostnew');
+	define('DB_PASS', 'Muruli143@');
+	define('DB_NAME', '3dpilot');
